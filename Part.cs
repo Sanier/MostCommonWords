f8273ef1e6385885fdae46bbbe12b1577e618334
Sanier <Sanier20@mail.ru>
2021-11-24 22:38:25 +0300

    Добавлена сортировка и вывод наиболее встречающихся 10 слов

---
+++ Part.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MostCommonWords
+{
+    public class Part : IEquatable<Part>, IComparable<Part>
+    {
+        public string Word { get; set; }
+
+        public int Val { get; set; }
+
+        public override string ToString()
+        {
+            return "Количество повторений: " + Val + "   Слово: " + Word;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            Part objAsPart = obj as Part;
+            if (objAsPart == null) return false;
+            else return Equals(objAsPart);
+        }
+        public int SortByNameAscending(string name1, string name2)
+        {
+
+            return name1.CompareTo(name2);
+        }
+
+        // Default comparer for Part type.
+        public int CompareTo(Part comparePart)
+        {
+            // A null value means that this object is greater.
+            if (comparePart == null)
+                return 1;
+
+            else
+                return this.Val.CompareTo(comparePart.Val);
+        }
+        public override int GetHashCode()
+        {
+            return Val;
+        }
+        public bool Equals(Part other)
+        {
+            if (other == null) return false;
+            return (this.Val.Equals(other.Val));
+        }
+        // Should also override == and != operators.
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -6,11 +7,34 @@ namespace MostCommonWords
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var list = new List<string>();
+
             string text = File.ReadAllText("C:\\Users\\sanie\\Desktop\\Text1.txt");
 
             var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            var words = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            list.AddRange(words);
+            list.Sort();
+
+            var ted = new List<Part>();
+
+            foreach (string val in list.Distinct().OrderBy(x => x))
+            {
+                int count = list.Count(x => x == val);
+                ted.Add(new Part { Word = val, Val = count });
+            }
+
+            ted.Sort();
+            ted.Reverse();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(ted[i]);
+            }
         }
     }
 }
