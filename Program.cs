06edeb14fb63b5ec5cc827e3390087db0260c0a9
Sanier <Sanier20@mail.ru>
2021-11-24 20:03:11 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace MostCommonWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("C:\\Users\\sanie\\Desktop\\Text1.txt");
+
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+        }
+    }
+}
